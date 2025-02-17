23d095c9cf8f95ac21477f6e3faff2306eb43b96
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-02-18 01:28:17 +0600

    refactor: update JansEmailService.java

--- projects/agama-magiclink/lib/org/gluu/agama/jans/service/JansEmailService.java
+++ projects/agama-magiclink/lib/org/gluu/agama/jans/service/JansEmailService.java
@@ -1,4 +1,4 @@
-package org.gluu.agama.smtp.jans.service;
+package org.gluu.agama.jans.service;
 
 import io.jans.as.common.model.common.User;
 import io.jans.as.common.service.common.EncryptionService;
