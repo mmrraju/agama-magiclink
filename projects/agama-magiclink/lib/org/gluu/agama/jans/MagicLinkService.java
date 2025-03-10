e733078ef5edd8431d45e24fa920940e41f2b09e
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-03-11 03:33:46 +0600

    feat: add MagicLinkService.java

---
+++ projects/agama-magiclink/lib/org/gluu/agama/jans/MagicLinkService.java
@@ -0,0 +1,16 @@
+package org.gluu.agama.jans;
+
+import java.util.Map;
+
+import org.gluu.agama.jans.model.ContextData;
+import org.gluu.agama.jans.service.Service;
+
+public abstract class MagicLinkService {
+    public abstract Map<String, String> getUserEntity(String email);
+
+    public abstract String sendMail(String to, ContextData context);
+
+    public static MagicLinkService getInstance(){
+        return Service.getInstance();
+    }
+}
