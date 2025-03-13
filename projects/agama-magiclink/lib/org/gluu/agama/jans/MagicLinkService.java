b6d532547a8cef4e41fb177af41f68deee46b212
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-03-14 00:57:34 +0600

    refactor: update MagicLinkService.java

--- projects/agama-magiclink/lib/org/gluu/agama/jans/MagicLinkService.java
+++ projects/agama-magiclink/lib/org/gluu/agama/jans/MagicLinkService.java
@@ -8,7 +8,7 @@ import org.gluu.agama.jans.service.Service;
 public abstract class MagicLinkService {
     public abstract Map<String, String> getUserEntity(String email);
 
-    public abstract String sendMail(String to, ContextData context);
+    public abstract String sendMail(String to, ContextData context) throws Exception;
 
     public abstract boolean verifyMagicLink(String token);
 
