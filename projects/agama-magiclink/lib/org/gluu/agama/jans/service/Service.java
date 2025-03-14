b6de435bddb09da651239b2f9ed8d579af6875cf
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-03-15 00:10:45 +0600

    refactor: update Service.java

--- projects/agama-magiclink/lib/org/gluu/agama/jans/service/Service.java
+++ projects/agama-magiclink/lib/org/gluu/agama/jans/service/Service.java
@@ -56,7 +56,7 @@ public class Service extends MagicLinkService{
 
     public String generateMagicLink(String token) throws Exception {
 
-        return HOST + "jans-auth/magic-link?token=" + token;
+        return HOST + "jans-auth/fl/callback?token=" + token;
     }
 
     public boolean verifyMagicLink(String token) {
